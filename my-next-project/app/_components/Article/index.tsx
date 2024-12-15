b2d6829c1441fb1d66d827e3ea587695171ca20a
Okayan <okayan0128@hotmail.com>
2024-12-15 22:27:16 +0900

    7章まで終わり

--- my-next-project/app/_components/Article/index.tsx
+++ my-next-project/app/_components/Article/index.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 import Image from "next/image";
 import type { News } from "@/app/_libs/microcms";
 import Date from "../Date";
 import Category from "../Category";
-import style from "./index.module.css";
+import styles from "./index.module.css";
 
 type Props = {
   data: News;
@@ -11,23 +12,28 @@ type Props = {
 export default function Article({ data }: Props) {
   return (
     <main>
-      <h1 className={style.title}>{data.title}</h1>
-      <p className={style.description}>{data.description}</p>
-      <div className={style.meta}>
-        <Category category={data.category} />
-        <Date date={data.publishedAt ?? data.createdAt} />
+      <h1 className={styles.title}>{data.title}</h1>
+      <p className={styles.description}>{data.description}</p>
+      <div className={styles.meta}>
+        <Link
+          href={`/news/category/${data.category.id}`}
+          className={styles.categoryLink}
+        >
+          <Category category={data.category} />
+        </Link>
+        <Date date={data.publishedAt || data.createdAt} />
       </div>
       {data.thumbnail && (
         <Image
           src={data.thumbnail.url}
           alt=""
-          className={style.thumbnail}
+          className={styles.thumbnail}
           width={data.thumbnail.width}
           height={data.thumbnail.height}
         />
       )}
       <div
-        className={style.content}
+        className={styles.content}
         dangerouslySetInnerHTML={{
           __html: data.content,
         }}
